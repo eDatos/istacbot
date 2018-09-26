7824c3a4f3d34fb6e93bbfa730a95d211dacadda
Isaac Aiman Salas <iaimsal@arte-consultores.com>
2018-09-26 12:08:41 +0100

    METAMAC-2847 - Modificar el comportamiento de los indicadores que tienen los mismos sinónimos
    > Eliminado script "extract_synonyms_from_nlu_train.py".

--- etc/extract_synonyms_from_nlu_train.py
+++
@@ -1,9 +0,0 @@
-import json
-
-with open('../data/nlu_train.json', mode="r", encoding="utf-8") as json_data:
-    nlu_train = json.load(json_data)
-
-    for synonym_entity in nlu_train["rasa_nlu_data"]["entity_synonyms"]:
-        print("\"" + synonym_entity["value"] + "\": ", end="")
-        print( synonym_entity["synonyms"], end=",\n")
-
