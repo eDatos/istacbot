daaf2669e88b009622d647bffbbeddbc7905454e
Isaac Aiman Salas <iaimsal@arte-consultores.com>
2018-09-28 14:08:31 +0100

    METAMAC-2847 - Modificar el comportamiento de los indicadores que tienen los mismos sinónimo
    > Indicadores sugeridos repetidos corregido.

--- actions.py
+++ actions.py
@@ -43,14 +43,13 @@ def initializeSynonyms():
     with open(properties.dataset_path + '/rasa_dataset_training.json', mode="r", encoding="utf-8") as json_data:
         nlu_train = json.load(json_data)
         for entity_synonyms in nlu_train["rasa_nlu_data"]["entity_synonyms"]:
-            if (entity_synonyms["value"] in indicators):
+            if (entity_synonyms["value"] in indicators and entity_synonyms["value"] not in synonyms_indexed_by_indicator.keys()):
                 synonyms_indexed_by_indicator[entity_synonyms["value"]] = entity_synonyms["synonyms"]
                 for synonym in entity_synonyms["synonyms"]:
                     indicators_indexed_by_synonym[synonym] = entity_synonyms["value"]
 
     # Todos los sinónimos son su propio sinónimo.
     for indicator in indicators.keys():
-        print(indicator)
         indicators_indexed_by_synonym[indicator] = indicator
 
 initializeSynonyms()
@@ -264,14 +263,15 @@ class ActionShow(Action):
         if (len(indicators_sorted) > NUMBER_SIMILAR_INDICATORS):
             indicators_sorted = indicators_sorted[:NUMBER_SIMILAR_INDICATORS]
 
-
         # Añadidos otros indicadores con el mismo sinónimo
         indicators_same_synonym = []
         for indicator in synonyms_indexed_by_indicator.keys():
-            if (indicator not in indicators_sorted):
-                for synonym in synonyms_indexed_by_indicator[indicator]:
-                    if (indicator_slot in synonyms_indexed_by_indicator and synonym in synonyms_indexed_by_indicator[indicator_slot] and indicator_slot != indicator):
-                        indicators_same_synonym.append(indicator)
+            for synonym in synonyms_indexed_by_indicator[indicator]:
+                if (indicator_slot in synonyms_indexed_by_indicator and synonym in synonyms_indexed_by_indicator[
+                    indicator_slot] and indicator_slot != indicator and indicator not in indicators_same_synonym):
+                    indicators_same_synonym.append(indicator)
+                    if (indicator in indicators_sorted):
+                        indicators_sorted.remove(indicator)
 
         indicators_sorted = indicators_same_synonym + indicators_sorted
 
